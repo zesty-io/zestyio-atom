eb2b46f53bae5b5ed0b19987ec3d97128c6388c5
ardeay <apuzzo@gmail.com>
2018-09-19 21:14:54 -0700

    upadate readme

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
+# !! Alpha: Not For Production Use
+
+Currently the Atom package will connect to your content instance and download all of the files. You can then edit the files and they will write directly to the development version of the file. To publish files you must be logged into the Zesty.io manager of that instance.
+
+Please help us complete this package by submitting feedback in the [zestyiodevs slack channel](https://chat.zesty.io/).
+
+---
+
 ## Zesty.io ATOM plugin
 
 Syncs Atom project to a Zesty.io Cloud Content Instance to sync and write files, and build Parsley autocomplete references.
 
 ## How to use
 
-The project need a file in the root directory named `zesty.json`
+Create a directory and start an empty project in it. Then create a file in the root directory named `zesty.json`
 
-Example contents to get started
+To get started, you need a JSON object that looks like:
 
 ```
 {
@@ -15,17 +23,13 @@ Example contents to get started
 }
 ```
 
-### Todos for alpha launch
+This information can be accessed from the Zesty.io Manager Code Editor tab. From inside that tab, there is a link in the object helper tray labeled "external editing". Click that tray option for this file.
 
-* Stylesheet and Scripts Saving
+### In the works for alpha
+
 * Have tray load information on the file opened
 * Have tray load relative information on the model associated with the views
 * Have tray show a publish button for the file
-
-### Todos for beta launch
-
-* parsley code highlighting (mixed html + handlebars)
-*
 
 ### Ideas
 
--- spec/zestyio-atom-plugin-spec.js
+++ spec/zestyio-atom-plugin-spec.js
@@ -1,43 +1,40 @@
 'use babel';
 
-import ZestyioAtomPlugin from '../lib/zestyio-atom-plugin';
+import ZestyioAtom from '../lib/zestyio-atom-plugin';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe('ZestyioAtomPlugin', () => {
+describe('ZestyioAtom', () => {
   let workspaceElement, activationPromise;
 
   beforeEach(() => {
     workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('zestyio-atom-plugin');
+    activationPromise = atom.packages.activatePackage('zestyio-atom');
   });
 
   describe('when the zestyio-atom-plugin:toggle event is triggered', () => {
     it('hides and shows the modal panel', () => {
       // Before the activation event the view is not on the DOM, and no panel
       // has been created
-      expect(workspaceElement.querySelector('.zestyio-atom-plugin')).not.toExist();
+      expect(workspaceElement.querySelector('.zestyio-atom')).not.toExist();
 
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'zestyio-atom-plugin:toggle');
 
       waitsForPromise(() => {
         return activationPromise;
       });
 
       runs(() => {
-        expect(workspaceElement.querySelector('.zestyio-atom-plugin')).toExist();
+        expect(workspaceElement.querySelector('.zestyio-atom')).toExist();
 
-        let zestyioAtomPluginElement = workspaceElement.querySelector('.zestyio-atom-plugin');
+        let zestyioAtomPluginElement = workspaceElement.querySelector('.zestyio-atom');
         expect(zestyioAtomPluginElement).toExist();
 
         let zestyioAtomPluginPanel = atom.workspace.panelForItem(zestyioAtomPluginElement);
         expect(zestyioAtomPluginPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'zestyio-atom-plugin:toggle');
+
         expect(zestyioAtomPluginPanel.isVisible()).toBe(false);
       });
     });
@@ -51,11 +48,8 @@ describe('ZestyioAtomPlugin', () => {
       // workspaceElement to the DOM are generally slower than those off DOM.
       jasmine.attachToDOM(workspaceElement);
 
-      expect(workspaceElement.querySelector('.zestyio-atom-plugin')).not.toExist();
+      expect(workspaceElement.querySelector('.zestyio-atom')).not.toExist();
 
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'zestyio-atom-plugin:toggle');
 
       waitsForPromise(() => {
         return activationPromise;
@@ -63,9 +57,9 @@ describe('ZestyioAtomPlugin', () => {
 
       runs(() => {
         // Now we can test for view visibility
-        let zestyioAtomPluginElement = workspaceElement.querySelector('.zestyio-atom-plugin');
+        let zestyioAtomPluginElement = workspaceElement.querySelector('.zestyio-atom');
         expect(zestyioAtomPluginElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'zestyio-atom-plugin:toggle');
+        atom.commands.dispatch(workspaceElement, 'zestyio-atom:toggle');
         expect(zestyioAtomPluginElement).not.toBeVisible();
       });
     });
